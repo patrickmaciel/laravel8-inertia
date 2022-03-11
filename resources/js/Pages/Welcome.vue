9173805c2f9b9c5f21a9a4ea9f0890fe2e08cbc3
Patrick Maciel <patrickmaciel.info@gmail.com>
2022-03-11 18:56:48 -0300

    course episodes 1~4

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <Nav />
+
+    <h1>Hello, {{ name }}</h1>
+    <p>In this series, we will use the following frameworks:</p>
+    <ul>
+        <li v-for="framework,key in frameworks" v-bind:key="key" v-text=framework></li>
+    </ul>
+    <hr>
+</template>
+
+<script>
+import Nav from '../Shared/Nav';
+
+export default {
+    components: { Nav },
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+};
+</script>
