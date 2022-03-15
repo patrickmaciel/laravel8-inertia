f361caf8d596a1115822989f252cf50d7c7e6230
Patrick Maciel <patrickmaciel.info@gmail.com>
2022-03-15 15:36:29 -0300

    active links

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,18 +1,22 @@
 <template>
   <nav class='mb-4'>
     <ul class='flex gap-2'>
-      <li><Link class='text-blue-500 hover:underline' href="/">Home</Link></li>
-      <li><Link class='text-blue-500 hover:underline' href="/users">Users</Link></li>
-      <li><Link class='text-blue-500 hover:underline' href="/settings">Settings</Link></li>
-      <li><Link class='text-blue-500 hover:underline' href="/logout" method='post' :data="{ foo: 'bar'}" as='button'>Log Out</Link></li>
+      <li><NavLink href='/' :class="{ 'font-bold underline' : $page.component == 'Welcome' }">Home</NavLink></li>
+      <li><NavLink href='/users' :class="{ 'font-bold underline' : $page.component == 'Users' }">Users</NavLink></li>
+      <li><NavLink href='/settings' :class="{ 'font-bold underline' : $page.component == 'Settings' }">Settings</NavLink></li>
+      <li><NavLink href='/logout' :class="{ 'font-bold underline' : $page.url == '/logout' }">Log Out</NavLink></li>
+      <!-- <li><Link class='text-blue-500 hover:underline' :class="{ 'font-bold underline': $page.url.startsWith('/users') }" href="/users">Users</Link></li>
+      <li><Link class='text-blue-500 hover:underline' :class="{ 'font-bold underline': $page.component == 'Settings' }" href="/settings">Settings</Link></li>
+      <li><Link class='text-blue-500 hover:underline' :class="{ 'font-bold underline': $page.url == '/logout' }" href="/logout" method='post' :data="{ foo: 'bar'}" as='button'>Log Out</Link></li> -->
     </ul>
   </nav>
 </template>
 
 <script>
-import { Link } from "@inertiajs/inertia-vue3";
+import NavLink from "./NavLink";
+
 export default {
-  components: { Link }
+  components: { NavLink }
 };
 </script>
 
