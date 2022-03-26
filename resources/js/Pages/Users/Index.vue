e35d625097f9076ba966c413787fc650e85619d7
Patrick Maciel <patrickmaciel.info@gmail.com>
2022-03-26 01:39:53 -0300

    import aliases

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -105,7 +105,7 @@
 </template>
 
 <script setup>
-import Pagination from "../../Shared/Pagination.vue";
+import Pagination from "@/Shared/Pagination.vue";
 import { ref, watch } from "vue";
 import { Inertia } from "@inertiajs/inertia";
 import throttle from "lodash/throttle";
